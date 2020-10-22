d60a5e4ba39bf2a28cf5eceab3b32d2f6876a007
olgeni <olgeni@olgeni.com>
2020-10-22 20:57:39 +0200

    Remove extra 'pkg autoremove'.

--- cloud-init/digital-ocean.sh
+++ cloud-init/digital-ocean.sh
@@ -43,7 +43,6 @@ git clone https://github.com/olgeni/bsdkit.git
 cd bsdkit
 git checkout ${BSDKIT_BRANCH}
 env BSDKIT_INSTALL_HOST=yes ansible-playbook --connection=local --inventory 127.0.0.1, -e ansible_python_interpreter=/usr/local/bin/python3 playbook/bsdkit.yml
-pkg autoremove -y
 
 if route get default | grep "interface:" > /dev/null 2>&1; then
     _iface=$(route get default | awk '/interface:/ { print $2 }')
