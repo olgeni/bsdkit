b12c66409808248b5d7b82b96ce7e81d88d65c76
olgeni <olgeni@olgeni.com>
2022-05-16 23:15:48 +0200

    Set default version to 13.1.

--- cloud-init/digitalocean.sh
+++ cloud-init/digitalocean.sh
@@ -5,7 +5,7 @@ set -e -u -x
 exec > /var/log/bsdkit-cloud-init.log 2>&1
 
 : "${BSDKIT_BRANCH:=master}"
-: "${BSDKIT_VERSION:=13.0}"
+: "${BSDKIT_VERSION:=13.1}"
 : "${BSDKIT_JAIL_NETWORK:=172.16.1.0/24}"
 : "${ZFS_SWAP_SIZE:=4G}"
 
